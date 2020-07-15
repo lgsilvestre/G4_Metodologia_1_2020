7fe0c21d0dc744f4319b8199d9f026aea5fec028
Joaquin Saavedra <joaq4699@hotmail.com>
2020-07-14 21:01:05 -0400

    Creadas las view de canvas y view. Creada la base de ViewModel

---
+++ CapsulaScript/CapsulaScript/ViewModel/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using CapsulaScript.MVVMHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapsulaScript.ViewModel
+{
+    public abstract class ViewModelBase : OnPropertyChangedBase
+    {
+    }
+}
