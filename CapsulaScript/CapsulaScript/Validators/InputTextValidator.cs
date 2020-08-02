f1d7e437abaa81006de2d631caee438ec1093fda
Joaquin Saavedra <joaq4699@hotmail.com>
2020-08-01 23:41:09 -0400

    Creado namespace para validadores de entrada

---
+++ CapsulaScript/CapsulaScript/Validators/InputTextValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CapsulaScript.Validators
+{
+    public class InputTextValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
