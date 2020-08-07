aecf673e7870d903dce2ab0b6f06ae8bf3068bc4
BastianB <bastianbarrera@gmail.com>
2020-08-07 16:43:07 -0400

    Intento de Dial, el resultado no es favorable, se opta por cambiar el tipo de control de los grados

---
+++ CapsulaScript/CapsulaScript/View/Dial.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CapsulaScript.View
+{
+    /// <summary>
+    /// Lógica de interacción para Dial.xaml
+    /// </summary>
+    public partial class Dial : UserControl, INotifyPropertyChanged
+    {
+        public Dial()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+        private void Grid_ManipulationDelta_1(object sender, ManipulationDeltaEventArgs e)
+        {
+            this.Angle = GetAngle(new Point(e.ManipulationOrigin.X, e.ManipulationOrigin.Y), this.RenderSize);
+            this.Amount = (int)(this.Angle / 360);
+        }
+        int m_Amount = default(int);
+        public int Amount { get { return m_Amount; } set { SetProperty(ref m_Amount, value); } }
+
+        double m_Angle = default(double);
+        public double Angle { get { return m_Angle; } set { } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void SetProperty<T>(ref T storage, T value, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            if (!object.Equals(storage, value))
+            {
+                storage = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public enum Quadrants : int { nw = 2, ne = 1, sw = 4, se = 3 }
+        private double GetAngle(Point touchPoint, Size circleSize)
+        {
+            var _X = touchPoint.X - (circleSize.Width / 2d);
+            var _Y = circleSize.Height - touchPoint.Y - (circleSize.Height / 2d);
+            var _Hypot = Math.Sqrt(_X * _X + _Y * _Y);
+            var _Value = Math.Asin(_Y / _Hypot) * 180 / Math.PI;
+            var _Quadrant = (_X >= 0) ?
+                (_Y >= 0) ? Quadrants.ne : Quadrants.se :
+                (_Y >= 0) ? Quadrants.nw : Quadrants.sw;
+            switch (_Quadrant)
+            {
+                case Quadrants.ne: _Value = 090 - _Value; break;
+                case Quadrants.nw: _Value = 270 + _Value; break;
+                case Quadrants.se: _Value = 090 - _Value; break;
+                case Quadrants.sw: _Value = 270 + _Value; break;
+            }
+            return _Value;
+        }
+
+    }
+}
